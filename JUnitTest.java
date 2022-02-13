332f27b413f104672f6975c0582f31d0559e140f
Preetam Sreedath Bhamidipati <71397414+prb55@users.noreply.github.com>
2022-02-12 21:44:10 -0500

    Add files via upload

--- JUnitTest.java
+++ JUnitTest.java
@@ -6,13 +6,13 @@ class JUnitTest {
 
 	@Test
 	void test_date09() throws Exception {
-		most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.csv", "2018-12-09");
+		most_active_cookie cookie = new most_active_cookie("cookie_log.csv", "2018-12-09");
 		assertEquals("AtY0laUfhglK3lC7", cookie.Jtests.get(0));
 	}
 	
 	@Test
 	void test_date08() throws Exception {
-		most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.csv", "2018-12-08");
+		most_active_cookie cookie = new most_active_cookie("cookie_log.csv", "2018-12-08");
 		assertEquals("SAZuXPGUrfbcn5UA", cookie.Jtests.get(0));
 		assertEquals("4sMM2LxV07bPJzwf", cookie.Jtests.get(1));
 		assertEquals("fbcn5UAVanZf6UtG", cookie.Jtests.get(2));
@@ -21,20 +21,20 @@ class JUnitTest {
 	
 	@Test
 	void test_date07() throws Exception {
-		most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.csv", "2018-12-07");
+		most_active_cookie cookie = new most_active_cookie("cookie_log.csv", "2018-12-07");
 		assertEquals("4sMM2LxV07bPJzwf", cookie.Jtests.get(0));
 	}
 	
 	@Test
 	void test_date06() throws Exception {
-		most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.csv", "2018-12-06");
+		most_active_cookie cookie = new most_active_cookie("cookie_log.csv", "2018-12-06");
 		assertTrue(cookie.Jtests.isEmpty());
 	}
 	
 	@Test
 	void test_incorrectFilePath() throws Exception {
 		try {
-			most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log12.csv", "2018-12-06");
+			most_active_cookie cookie = new most_active_cookie("cookie_log12.csv", "2018-12-06");
 		} catch (Exception e) {
 			assertTrue(e.toString().contains("Invalid file path. Please make the necessary changes and re-run!"));
 		}
@@ -43,7 +43,7 @@ class JUnitTest {
 	@Test
 	void test_incorrectFileType() throws Exception {
 		try {
-			most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.tsv", "2018-12-06");
+			most_active_cookie cookie = new most_active_cookie("cookie_log.tsv", "2018-12-06");
 		} catch (Exception e) {
 			assertTrue(e.toString().contains("Invalid date or file type. Please make the necessary changes and re-run!"));
 		}
@@ -52,7 +52,7 @@ class JUnitTest {
 	@Test
 	void test_incorrectDateSize() throws Exception {
 		try {
-			most_active_cookie cookie = new most_active_cookie("C:\\Users\\preeb\\Computer Science\\workspace\\QuantCast\\src\\cookie_log.csv", "2018-12");
+			most_active_cookie cookie = new most_active_cookie("cookie_log.csv", "2018-12");
 		} catch (Exception e) {
 			assertTrue(e.toString().contains("Invalid date or file type. Please make the necessary changes and re-run!"));
 		}
